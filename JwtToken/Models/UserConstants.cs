e2d7c063dbd822d6e0c7bc87918c01e105ce65db
Erencem <82844580+erencemozturk@users.noreply.github.com>
2023-04-23 12:48:29 +0300

    Add files via upload

---
+++ JwtToken/Models/UserConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtToken.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new()
+            {
+                    new UserModel(){ Username="erencem",Password="admin1",Role="Admin"}
+            };
+    }
+}
